e5326cde614db7c2915948a4f4ec133c467eea06
kelu124 <kelu124@gmail.com>
2023-11-22 09:22:30 +0100

    Update

---
+++ app_tools.py
@@ -0,0 +1,56 @@
+default_tools = [
+    {
+            "name": "get_current_weather",
+            "description": "Get the current weather",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "location": {
+                        "type": "string",
+                        "description": "The city and state, e.g. San Francisco, CA",
+                    },
+                    "format": {
+                        "type": "string",
+                        "enum": ["celsius", "fahrenheit"],
+                        "description": "The temperature unit to use. Infer this from the users location.",
+                    },
+                },
+                "required": ["location", "format"],
+            },
+        },
+    {
+            "name": "get_n_day_weather_forecast",
+            "description": "Get an N-day weather forecast",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "location": {
+                        "type": "string",
+                        "description": "The city and state, e.g. San Francisco, CA",
+                    },
+                    "format": {
+                        "type": "string",
+                        "enum": ["celsius", "fahrenheit"],
+                        "description": "The temperature unit to use. Infer this from the users location.",
+                    },
+                    "num_days": {
+                        "type": "integer",
+                        "description": "The number of days to forecast",
+                    }
+                },
+                "required": ["location", "format", "num_days"]
+            },
+        }
+
+]
+
+
+
+default_messages = [{'role': 'system',
+  'content': "Don't make assumptions about what values to plug into functions. Ask for clarification if a user request is ambiguous.\nDon't make assumptions about what values to plug into functions. Ask for clarification if a user request is ambiguous."},
+ {'role': 'user', 'content': "What's the weather like today in Dublin"},
+ {'role': 'assistant',
+  'content': None,
+  'function_call': {'name': 'get_current_weather',
+   'arguments': '{"location":"Dublin","format":"celsius"}'}},
+ {'role': 'user', 'content': 'The city is Dublin'}]
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 import os
 from pydantic import BaseModel
 import OAI
+import app_tools
+from typing import List
+
 
 app = FastAPI()
 is_prod = os.environ.get('IS_HEROKU', None) 
@@ -13,6 +16,21 @@ class RequestAsk(BaseModel):
     token: str = "TOK3N"
     overwrite: bool = False
 
+class RequestFct(BaseModel):
+    context: str = "Don't make assumptions about what values to plug into functions. Ask for clarification if a user request is ambiguous."
+    question: str = "What's the weather like today"
+    model: str = "gpt-3.5-turbo-1106"
+    functions : List[dict] = app_tools.default_tools
+    overwrite: bool = False
+    token: str = "TOK3N"
+
+class RequestFctEngine(BaseModel):
+    messages: List[dict] = app_tools.default_messages
+    model: str = "gpt-3.5-turbo-1106"
+    functions : List[dict] = app_tools.default_tools
+    overwrite: bool = False
+    token: str = "TOK3N"
+
 @app.post("/ask/")
 async def ask(itemR: RequestAsk):
 
@@ -23,6 +41,32 @@ async def ask(itemR: RequestAsk):
     else:
         ans = "Incorrect token "
     return {"answer":ans}
+
+
+
+@app.post("/fct/")
+async def fct(itemR: RequestFct):
+    if (itemR.token == os.environ.get('TOKEN')) or (not is_prod):
+        f = OAI.askFCT("demo_fct_fastapi","./cache")
+        print("OVERWRITE",itemR.overwrite)
+        messages, chat_response = f.askFct(itemR.context,itemR.question,itemR.functions,modelGPT=itemR.model,ow=itemR.overwrite)
+        #ans = h.ask(itemR.context,itemR.question,v=itemR.model,ow=itemR.overwrite)
+    else:
+        messages = {}
+        chat_response = ""
+    return {"messages":messages,"answer":chat_response}
+
+@app.post("/fctEngine/")
+async def fctEngine(itemR: RequestFctEngine):
+    if (itemR.token == os.environ.get('TOKEN')) or (not is_prod):
+        f = OAI.askFCT("demo_fctengine_fastapi","./cache")
+        print("OVERWRITE",itemR.overwrite)
+        msgs, answer = f.askFtcEngine(itemR.messages,itemR.functions,modelGPT=itemR.model,ow=itemR.overwrite)
+        #ans = h.ask(itemR.context,itemR.question,v=itemR.model,ow=itemR.overwrite)
+    else:
+        msgs = {}
+        answer = ""
+    return {"messages":msgs,"answer":answer}
 
 
 @app.get("/")
