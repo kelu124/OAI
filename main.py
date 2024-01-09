a99ab2e80a99269414f36ca7bfc9d5b2ac8f74cf
kelu124 <kelu124@gmail.com>
2024-01-09 14:34:48 +0100

    Adding seed option

--- main.py
+++ main.py
@@ -75,6 +75,7 @@ class RequestAsk(BaseModel):
     token: str = "TOK3N"
     overwrite: bool = False
     source: str = "api"
+    seed: str = ""
 
 class RequestFct(BaseModel):
     context: str = "Don't make assumptions about what values to plug into functions. Ask for clarification if a user request is ambiguous."
@@ -84,7 +85,8 @@ class RequestFct(BaseModel):
     overwrite: bool = False
     token: str = "TOK3N"
     source: str = "api"
-
+    seed: str = ""
+    
 class RequestFctEngine(BaseModel):
     messages: List[dict] = default_messages
     model: str = "gpt-3.5-turbo-1106"
@@ -92,6 +94,7 @@ class RequestFctEngine(BaseModel):
     overwrite: bool = False
     token: str = "TOK3N"
     source: str = "api"
+    seed: str = ""
 
 @app.post("/ask/")
 async def ask(itemR: RequestAsk):
@@ -99,7 +102,7 @@ async def ask(itemR: RequestAsk):
     if (itemR.token == os.environ.get('TOKEN')) or (not is_prod):
         h = OAI.Helper("fastapi","./cache")
         print("OVERWRITE",itemR.overwrite)
-        ans = h.ask(itemR.context,itemR.question,v=itemR.model,ow=itemR.overwrite,src=itemR.source)
+        ans = h.ask(itemR.context,itemR.question,v=itemR.model,ow=itemR.overwrite,src=itemR.source,seed=itemR.seed)
     else:
         ans = "Incorrect token "
     return {"answer":ans}
@@ -111,7 +114,7 @@ async def fct(itemR: RequestFct):
     if (itemR.token == os.environ.get('TOKEN')) or (not is_prod):
         f = OAI.askFCT("demo_fct_fastapi","./cache")
         print("OVERWRITE",itemR.overwrite)
-        messages, chat_response = f.askFct(itemR.context,itemR.question,itemR.functions,modelGPT=itemR.model,ow=itemR.overwrite,src=itemR.source)
+        messages, chat_response = f.askFct(itemR.context,itemR.question,itemR.functions,modelGPT=itemR.model,ow=itemR.overwrite,src=itemR.source,seed=itemR.seed)
         #ans = h.ask(itemR.context,itemR.question,v=itemR.model,ow=itemR.overwrite)
     else:
         messages = {}
@@ -123,7 +126,7 @@ async def fctEngine(itemR: RequestFctEngine):
     if (itemR.token == os.environ.get('TOKEN')) or (not is_prod):
         f = OAI.askFCT("demo_fctengine_fastapi","./cache")
         print("OVERWRITE",itemR.overwrite)
-        msgs, answer = f.askFtcEngine(itemR.messages,itemR.functions,modelGPT=itemR.model,ow=itemR.overwrite,src=itemR.source)
+        msgs, answer = f.askFtcEngine(itemR.messages,itemR.functions,modelGPT=itemR.model,ow=itemR.overwrite,src=itemR.source,seed=itemR.seed)
         #ans = h.ask(itemR.context,itemR.question,v=itemR.model,ow=itemR.overwrite)
     else:
         msgs = {}
